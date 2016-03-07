4e9a6f3a117ce95eddfa3d93eea5d8e551583e14
20131105796MCR <a20131105796@admindeiMac-08.local>
2016-03-07 21:13:00 +0800

    1。变量和常量

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,43 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+Int.min
+Int.max
+
+Int64.min
+Int64.max
+
+let fifteenInDecimal = 15
+let fifteenInHex = 0xF
+let fifteenInOctal = 0o17
+let fifteenInBinary = 0b1111
+
+var oneThirdInFloat: Float = 1/3
+var oneThirdInDouble: Double = 1/3
+
+print(oneThirdInFloat)
+print(oneThirdInDouble)
+
+var PI = 0.314e1
+PI = 314e-2
+
+var three = 3
+three.dynamicType
+
+var zeroPointForteen = 0.14
+zeroPointForteen.dynamicType
+
+
+let emptyString = ""
+let emptyString1 = String()
+
+var swfit = "Swift is fun."
+
+let swfitCopy = swfit
+print(swfit)
+
+let charA:Character = "A"
+let chaB = "B"
+
---
+++ main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  new01
+//
+//  Created by 20131105796MCR on 16/3/7.
+//  Copyright © 2016年 20131105796MCR. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
